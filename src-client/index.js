2692df67c34e8847e91f1896e2ff7be4f28e5593
Travis Hubbard <t3patterson@gmail.com>
2017-03-23 13:19:13 -0400

    removing link tag in dev environment

--- src-client/index.js
+++ src-client/index.js
@@ -1,2 +1,9 @@
+if(window.location.hostname === 'localhost'){
+	let linkEl = document.querySelector('link[href="./css/styles.css"]')
+	let headEl = document.querySelector('head')
+	headEl.removeChild(linkEl)
+}
+
 import styles from './styles/main.scss'
 import app from './scripts/app.js'
+
